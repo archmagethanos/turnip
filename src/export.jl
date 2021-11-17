7266886229af3286e92d9d6f68656104501e216d
archmagethanos@gmail.com <65360214+archmagethanos@users.noreply.github.com>
2021-11-16 23:26:23 -0700

    Added in some status updates

--- src/export.jl
+++ src/export.jl
@@ -1,5 +1,7 @@
 using DelimitedFiles
 using BenchmarkTools
+#using Base.Threads.@spawn
+
 include("polyroots.jl")
 
 # placeholder, this should actually do things
@@ -15,10 +17,12 @@ end
 
 function generateRoots()
     qRootsMatrix, headers = loadData("q_to_denom_30.csv")
-    roots = polyRoots(qRootsMatrix, headers)
-    open("data/foo.csv","w") do f
+    roots = polyRoots(qRootsMatrix, headers) # Calculate roots
+    println("Exporting Roots to CSV:")
+    open("data/q_to_roots_30.csv","w") do f
         write(roots, formatter, f)
     end
 end
 
-@btime generateRoots()
+# @btime generateRoots()
+generateRoots()
--- src/polyroots.jl
+++ src/polyroots.jl
@@ -1,13 +1,14 @@
-using Pkg; Pkg.add("DoubleFloats")
-
 using PolynomialRoots
 using DelimitedFiles
-using BenchmarkTools
 using DoubleFloats
+using ProgressBars
+
+using BenchmarkTools
 
 # Our program takes an input of a .csv file of formatted vectorrs with the first row devoted to the header, 
 # a string representation of the rational vector composing the polynomial. The program exports the resulting
 # roots to a csv in the same directory. 
+
 
 function loadData(filename)
     datadirpath = "data/"
@@ -34,16 +35,18 @@ end
 function polyRoots(qRootsSource, headers)
     global rootsdict = Dict(headers[1]=>[], headers[2]=>[1+0im], headers[3]=>[1+0im], headers[4]=>[0+0im])
     n = 1
-    for col in eachcol(qRootsSource)
+    for col in ProgressBar(eachcol(qRootsSource))
         if get(rootsdict, headers[n], 0) == 0
            r = roots(col)
            rootsdict[headers[n]] = r
         end
-        n += 1
-    end  
+        n = n+1
+    end 
+    println("Roots Generation Complete.") 
     return rootsdict
 end 
 
-qRootsMatrix, headers = loadData("q_to_denom_30.csv")
-@btime roots = polyRoots(qRootsMatrix, headers)
 
+qRootsMatrix, headers = loadData("q_to_denom_30.csv")
+
+#@btime roots = polyRoots(qRootsMatrix, headers) # Calculate roots
