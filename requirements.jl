266856db22e30b27094df0bd194a21180c4ccb55
Victor Boyer <archmagethanos@gmail.com>
2022-01-06 10:49:53 -0700

    Pushing laptop changes

--- requirements.jl
+++ requirements.jl
@@ -3,5 +3,7 @@ Pkg.add("CSV")
 Pkg.add("DelimitedFiles")
 Pkg.add("BenchmarkTools")
 Pkg.add("DoubleFloats")
-Pkg.add("PolynomialRoots
+Pkg.add("PolynomialRoots")
 Pkg.add("Distributed")
+Pkg.add("ProgressMeter")
+Pkg.add("ProgressBars")
