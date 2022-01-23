befd44a677b153f170277acf3ec14a238917def3
archmagethanos <archmagethanos@gmail.com>
2022-01-22 22:08:33 -0700

    Organizational changews

--- src/roots/main.jl
+++ src/roots/main.jl
@@ -3,6 +3,10 @@ include("export.jl")
 
 #generateQ = pyimport("src/polynomials/generate_q.py")
 
-max_denom = "400"
+max_denom = "200"
 
-generateRoots("q_to_denom_" * max_denom,"q_to_roots_" * max_denom, "csv")
+generateRoots("q_to_denom_" * max_denom, "q_to_roots_" * max_denom, "csv")
+
+max_denom = "750"
+
+generateRoots("q_to_denom_" * max_denom, "q_to_roots_" * max_denom, "csv")
