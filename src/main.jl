f5e89520bad50794ea21783e07e33caa2504e4a3
archmagethanos <archmagethanos@gmail.com>
2022-01-23 22:26:23 -0700

    Fixed main

--- src/main.jl
+++ src/main.jl
@@ -6,7 +6,7 @@ include("plotting/plots.jl")
 # LEGACY: implement python call to generate roots
 # generateQ = pyimport("src/polynomials/generate_q.py")
 println(wd)
-function qPolyRootsScript(max_denom::Int; exportType::String, plotType::String)
+function qPolyRootsScript(max_denom::Int; exportType::String="none", plotType::String="none")
     max_denom = string(max_denom)
     infname::String = "data/q_to_denom_" * max_denom * ".csv"
     ofname::String = "data/q_to_roots_" * max_denom
@@ -14,9 +14,10 @@ function qPolyRootsScript(max_denom::Int; exportType::String, plotType::String)
     try 
         qRootsMatrix, headers = loader(infname)
         rootsDict = polyRoots(qRootsMatrix, headers)
-        exportType == "csv" | "jld2" && exportDict(rootsDict, ofname, exportType)
 
-        plotType == "png" && qPlot(rootsDict, plotType)
+        exportType != "none" && exportDict(rootsDict, ofname, exportType)
+
+        plotType != "none" && qPlot(rootsDict, plotType)
         
         println("Successful generation for roots to a maximum of " * max_denom * "!")
     catch e
@@ -26,8 +27,8 @@ function qPolyRootsScript(max_denom::Int; exportType::String, plotType::String)
 end
 
 function main()
-    qPolyRootsScript(30; "csv")
-    qPolyRootsScript(200; "csv")
+    qPolyRootsScript(30; "csv", "none")
+    qPolyRootsScript(200; "csv", "none")
     #qPolyRootsScript(400; "csv")
 end
 
