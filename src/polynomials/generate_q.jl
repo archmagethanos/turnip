653fbb44b7f6511770f7ccd4e3e9170e35f235b5
victor <archmagethanos@gmail.com>
2022-01-31 07:16:07 -0700

    Adding in new plotting changes

--- src/polynomials/generate_q.jl
+++ src/polynomials/generate_q.jl
@@ -2,22 +2,56 @@ using Polynomials
 using ContinuedFractions
 
 
-function corner()
-    #def corner(c): # Left and right corners of Triangle(c)
-    p, q, left, right, s = c[0], c[1], x, x, 1
-    while left==x or right==x: 
-        if left==x and  ((p*s-1)%q)==0:  left=vector([(p*s-1)/q,s])
-        if right==x and ((p*s+1)%q)==0:  right=vector([(p*s+1)/q,s])
-        s+=1
-    return [left, right]
+
+quot(v)=v[1]//v[2]
+vect(r)=[numerator(r),denominator(r)]
+
+
+remainder(v)=v[1]%v[2]
+flr(v)=numerator(floor(v[1]//v[2]))
+newpair(v)=[v[2],remainder(v)]
+function continued_fraction(v)
+	cf=[]
+	while remainder(v)!=0
+			push!(cf,flr(v))
+			v=newpair(v)
+	end
+	push!(cf,flr(v))
+	cf
 end
 
-function listQ()
+function cf_value(cf)
+	rev=reverse(cf)
+	rat=popfirst!(rev)
+    for j in rev
+		rat=j+1//rat
+	end
+	[numerator(rat),denominator(rat)]
 end
 
-function dQ()
+function center_corner_index(v)
+	v[1]==1 && return [[0,1],[1,0],v[2]]
+	center_cf=continued_fraction(v)[1:end-1]
+	center=cf_value(center_cf)
+	push!(center_cf,1)
+	k2=cf_value(center_cf)
+	corner=k2-center
+	ind=numerator((v[1]-corner[1])//center[1])
+	[center,corner,ind]
 end
 
-function Q()
+begin
+	function dQ(v)
+		return Polynomial(push!(zeros(Int64,v[2]), (-1)^v[1]))
+	end
+	function Q(v)
+		v==[0,1] && return 1
+		v==[1,0] && return 0
+		v==[1,1] && return 1
+		cci=center_corner_index(v)
+		c,k,i=cci[1],cci[2],cci[3]
+		return ([0 1; -dQ(c) Q(c)]^(i-1) * [Q(k); Q(k+c)])[2]
+	end
 end
+
 
